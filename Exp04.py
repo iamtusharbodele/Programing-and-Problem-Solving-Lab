934c87b6323dae31a737193b482f73cbc3a9fc3c
Tushar Bodele <tusharbodele090@gmail.com>
2025-05-06 16:44:03 +0530

    Create Exp04.py

---
+++ Exp04.py
@@ -0,0 +1,5 @@
+#Write a Python program to print the area of a triangle.
+base = float(input("Base: "))
+height = float(input("Height: "))
+area = (1/2) * base * height
+print("Area: {:.2f}".format(area))
